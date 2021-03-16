0fdfdd95e645ccb458a6de10d9136f81b8f29c6d
q <mats.bohlinsson@gmail.com>
2021-03-16 21:11:00 +0100

    Cannot convert bytes object to java.lang.String

--- app/src/main/python/hello.py
+++ app/src/main/python/hello.py
@@ -3,6 +3,12 @@ from androidx.appcompat.app import AppCompatActivity
 from com.chaquo.python.hello import R
 from java import jvoid, Override, static_proxy
 
+from flask import Flask
+app = Flask(__name__)
+@app.route('/')
+def hello():
+    return "Hello World!"
+
 
 class MainActivity(static_proxy(AppCompatActivity)):
 
@@ -10,3 +16,5 @@ class MainActivity(static_proxy(AppCompatActivity)):
     def onCreate(self, state):
         AppCompatActivity.onCreate(self, state)
         self.setContentView(R.layout.activity_main)
+        app.run()
+
